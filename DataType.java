c591dd40345103c739b37a0e3de52d8a6e6d82df
pranavrjb <pranav.rjb79@gmail.com>
2025-09-07 12:24:53 +0545

    Practicing Data type

---
+++ DataType.java
@@ -0,0 +1,20 @@
+class DataType{
+    public static void main(String a[]){
+        int num=4;
+
+        byte by=122;
+    
+        short sh=558;
+    
+        long l=12345671111l;
+    
+        float f=11.11f;
+    
+        double d=89.98;
+    
+        boolean b= true;
+    
+        char c='k';
+
+    }
+}
