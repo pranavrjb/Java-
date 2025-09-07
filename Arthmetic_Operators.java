de0f071d6a1a959051cfbdbaf7f3de951e647afc
pranavrjb <pranav.rjb79@gmail.com>
2025-09-07 13:00:58 +0545

    Arithemtic_Operators in java

---
+++ Arthmetic_Operators.java
@@ -0,0 +1,13 @@
+class Arithmetic_Operators{
+    public static void main(String[] args) {
+        int num=5;
+        // num +=1;
+        // num ++; //post increment
+        // ++num;  //pre increment
+        // num--;
+        int result=num++; //fetch the value and then increment
+        // int result= ++num; //increment and then fetch the value
+        System.out.println(result);
+
+    }
+}
