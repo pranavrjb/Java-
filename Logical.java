ed0e3d54ff6948aada34d1070639520e13dac68d
pranavrjb <pranav.rjb79@gmail.com>
2025-09-07 14:56:58 +0545

    #

---
+++ Logical.java
@@ -0,0 +1,11 @@
+class Logical{
+    public static void main(String[] args) {
+        int x=7;
+        int y=4;
+        int a=3;
+        int b=8;
+        boolean result=x > y && a < b;
+        System.out.println(result);
+    }
+
+}
