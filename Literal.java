ff1c50cba57e5e34ba6c1fedb2ad2243db2598ee
pranavrjb <pranav.rjb79@gmail.com>
2025-09-07 12:33:07 +0545

    literals

---
+++ Literal.java
@@ -0,0 +1,8 @@
+class Literal{
+    public static void main(String[] args) {
+        //Literals in java are just a fixed values that we write directly in our code, like numbers, text or true/false values. They are constants, meaning their value doesnt chnange while the program execute.
+    char c='a';
+    c++;
+    System.out.println(c);
+    }
+}
