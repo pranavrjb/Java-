3d19d3f48a28b4787308316fc293dd30ad383eaf
pranavrjb <pranav.rjb79@gmail.com>
2025-09-07 14:51:44 +0545

    Relational Operators

---
+++ Relation_Operators.java
@@ -0,0 +1,16 @@
+class Relation_Operators{
+    public static void main(String[] args) {
+        int x=5;
+        int y=8;
+        int a=1;
+        int b=1;
+        double d=33.3;
+        double d1=31.332;
+        boolean result = x > y;
+        boolean res= a==b;
+        boolean doub=d>d1;
+        System.out.println(result);
+        System.out.println(res);
+        System.out.println(doub);
+    }
+}
