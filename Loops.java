27197efec62137ac0b7760d0b0b92a6a98c1ad88
pranavrjb <pranav.rjb79@gmail.com>
2025-09-07 21:18:53 +0545

    Learning Loops in Java

---
+++ Loops.java
@@ -0,0 +1,32 @@
+public class Loops{
+    public static void main(String[] args) {
+        //loop -> while, do while, for, foreach
+        //while loop
+
+        // int i=1;
+
+        // while(i<=4){
+        //     System.out.println("Hello: "+i);
+        //     //Nested while loop
+        //     int j=1;
+        //     while (j<=3) {
+        //         System.out.println("Hi: "+j);
+        //         j++;
+        //     }
+        //     i++;
+        // }
+
+        //do while loop
+        // int i=5;
+        // do {
+        //     System.out.println("Hello");
+        // } while (i<=4);
+        // i++;
+        // System.out.println("Bye: "+i);
+
+        //for loop
+        for (int i=1;i<=4;i++){
+            System.out.println("Day: "+i);
+        }
+    }
+}
