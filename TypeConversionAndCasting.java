fb913082a3798b80aaa225028e46ac2626afee1d
pranavrjb <pranav.rjb79@gmail.com>
2025-09-07 12:51:36 +0545

    Type Conversion and Casting in java

---
+++ TypeConversionAndCasting.java
@@ -0,0 +1,18 @@
+class TypeConversionAndCasting{
+    public static void main(String[] args) {
+    //Implicit Casting
+    int num=10;
+    double d=num;
+    // Here, int is $ bytes and Int automatically converted to double i.e 8 byte.
+
+    System.out.println("Interger Value: "+ num);
+    System.out.println("Double vlaue {after implicit}: "+d);
+
+    //Casting or Explicit Casting
+    double d1=9.89;
+    int n=(int)d1;
+    System.out.println("Double Value: "+d1);
+    System.out.println("Integer value after explicit: "+n);
+        
+    }
+}
