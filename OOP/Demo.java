c6f2b2436a7a696a41da949aab807044dbdc60c2
pranavrjb <pranav.rjb79@gmail.com>
2025-09-08 09:43:08 +0545

    #

---
+++ OOP/Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+   public static void main(String[] args) {
+    StringBuffer sb=new StringBuffer("Jenny");
+    sb.append(" Wakler");
+    System.out.println(sb);
+    System.out.println("Jenny" + "Wakler");
+   } 
+}
