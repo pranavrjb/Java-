8d34ff34e0b582bf48ea982fe350cc29f932481d
pranavrjb <pranav.rjb79@gmail.com>
2025-09-08 09:20:36 +0545

    learned about array of obj and foreach loop in java

---
+++ OOP/ArrayOfObj.java
@@ -0,0 +1,31 @@
+class Student{
+    String Name;
+    int RollNo;
+    int marks;
+
+}
+public class ArrayOfObj {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.Name="Cartier";
+        s1.RollNo=15;
+        s1.marks=50;
+         Student s2=new Student();
+        s2.Name="Ram";
+        s2.RollNo=11;
+        s2.marks=66;
+         Student s3=new Student();
+        s3.Name="Jenny";
+        s3.RollNo=25;
+        s3.marks=76;
+
+        Student[] s=new Student[3];
+        s[0]=s1;
+        s[1]=s2;
+        s[2]=s3;
+        for (int i=0;i<s.length;i++){
+            System.out.println(s[i].Name + ":"+s[i].RollNo+"="+s[i].marks);
+        }
+
+    }
+}
