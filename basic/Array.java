f9bd5b9a9cf15ffe0d8dd6e9540d11dbccf647d7
pranavrjb <pranav.rjb79@gmail.com>
2025-09-08 08:37:35 +0545

    commented on array.java

--- basic/Array.java
+++ basic/Array.java
@@ -1,5 +1,7 @@
 public class Array {
     public static void main(String[] args) {
+
+        //Array have fixed size.
         int arr[]={1,2,3,4,5};
         System.out.println(arr[3]);
         //out: 4
